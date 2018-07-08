720127fdfe66f57b99ff0f9e2ee0f23d24dd4979
ArminBlaze <box_for_spam@mail.ru>
2018-07-08 18:06:49 +0300

    05 Done

--- js/form.js
+++ js/form.js
@@ -99,8 +99,30 @@
 		timein.value = timeout.value = this.value;
 	}
 	
+	function setAddress (position) {
+		address.value = "x: " + position.x + ", y: " + position.y;
+//		testAddress(position);
+	}
+	
+	//тестовая функция. Не нужна в финальной сборке. Отображает маркер на полученных координатах.
+	function testAddress (position) {
+		console.log(position);
+		var div = document.createElement('div');
+		div.style.position = 'absolute';
+		div.style.left = position.x + "px";
+		div.style.top = position.y + "px";
+		div.style.width = "1px";
+		div.style.height = "1px";
+		div.style.background = "lime";
+		div.style.outline = "1px solid black";
+		div.style.zIndex = "1001";
+		
+		window.map.mapElem.appendChild(div)
+	}
+	
 	window.form = {
 		noticeForm: noticeForm,
-		fieldsets: fieldsets
+		fieldsets: fieldsets,
+		setAddress: setAddress
 	};
 })();
--- js/map.js
+++ js/map.js
@@ -18,36 +18,33 @@
 	};
 	
 	
-	
-	
 	mainPin.addEventListener('mouseup', onMainPinMouseUp);
 	mapPins.addEventListener('click', onMapPinsClick);
+	
+	
+
 
 
 	function onMainPinMouseUp (e) {
-		if(window.mapShowed) return;
-		
 		showMap();
 		window.map.pin.drawButtons();
-		
+		mainPin.style.zIndex = 1000; // показывать над другими элементами
 		//убирать слушателья mouseup?
+		mainPin.removeEventListener('mouseup', onMainPinMouseUp);
+		mainPin.addEventListener('mousedown', onDragstart);
 	}
 
 	function onMapPinsClick (e) {
 		var target = e.target;
-
 		while (target != this) {
 			if ( target.classList.contains("map__pin") ) break;
 			target = target.parentNode;
 		}
 		if (target == this) return;
-
-		if(window.map.activePin) {
-			window.map.pin.deselectPin();
-		}
-
-		window.map.pin.selectPin(target);
 		
+
+		if(window.map.activePin) window.map.pin.deselectPin();
+		window.map.pin.selectPin(target);
 		window.map.card.showCard(target);
 	}
 	
@@ -58,8 +55,92 @@
 		window.form.fieldsets.forEach(function(item){
 			item.removeAttribute('disabled');
 		});
-		//
 		window.mapShowed = true;
+	}
+	
+	
+	function onDragstart (e) {
+		var mapCoords = getCoords(map);
+		var mapMax = {
+			width: map.clientWidth - mainPin.offsetWidth,
+			height: map.clientHeight - mainPin.offsetHeight
+		}
+		
+		e.preventDefault();
+		var coords = getCoords(mainPin);
+		
+// запоминаем место клика по элементу и учитываем координаты родителя-карты,
+// т.к. будем задавать пину относительные координаты, а не абсолютные
+		var shiftX = e.pageX - coords.left + mapCoords.left;
+		var shiftY = e.pageY - coords.top + mapCoords.top;
+		
+//		 подготовить к перемещению
+//		 2. разместить на том же месте, но в абсолютных координатах
+		mainPin.style.position = 'absolute';
+		mainPin.style.transform = "none";
+		onMouseMove(e);
+		
+		
+		
+//		 3, перемещать по экрану
+		document.addEventListener('mousemove', onMouseMove);
+
+		// 4. отследить окончание переноса
+		document.addEventListener('mouseup', onMouseUp);
+		
+		function onMouseMove(e) {
+			var left = e.pageX - shiftX;
+			var top = e.pageY - shiftY;
+			
+			//Ограничиваем координаты: Y min = 100, Y max = 500
+			var yMin = 100;
+			var yMax = 600;
+			
+			if(left < 0) left = 0;
+			else if (left > mapMax.width) left = mapMax.width;
+			if(top < yMin) top = yMin;
+			else if (top > yMax) top = yMax;
+//			else if (top > mapMax.height) top = mapMax.height;
+
+			mainPin.style.left = left + 'px';
+			mainPin.style.top = top + 'px';
+		}
+		
+		function onMouseUp () {
+			document.removeEventListener('mousemove', onMouseMove);
+			document.removeEventListener('mouseup', onMouseUp);
+			
+			calculatePosition();
+		}
+		
+		function calculatePosition () {
+			
+			var mainPinOffset = {
+				x: 32,
+				y: 82
+			};
+			
+			var position = {
+				x: +mainPin.style.left.slice(0, -2) + mainPinOffset.x,
+				y: +mainPin.style.top.slice(0, -2) + mainPinOffset.y
+			};
+			
+			
+			window.form.setAddress(position);
+		}
+		
+		mainPin.ondragstart = function() {
+			return false;
+		};
+		
+		function getCoords(elem) {   // кроме IE8-
+			var box = elem.getBoundingClientRect();
+			return {
+				top: box.top + pageYOffset,
+				left: box.left + pageXOffset
+			};
+		}
+	
 	}
 
 })();
